b2976f8b13f21a31f6f291089a053c3979d462e4
raguigmohammedamine <raguigmohammedamine@gmail.com>
2025-05-22 16:02:28 +0100

    MG-1 - Implement add function

---
+++ Level1.java
@@ -0,0 +1,9 @@
+Class Level1{
+    public static void main(String[] args) {
+        System.out.println("Hello Level 1");
+    }
+
+    private int add(int a, int b) {
+        return a + b;
+    }
+}
