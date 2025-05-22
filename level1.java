944e9041ad5278347ea0b149263855524f4bc84a
yasser <yasserfakir3@gmail.com>
2025-05-22 16:05:09 +0100

    MG-1 - Implement Add function

---
+++ level1.java
@@ -0,0 +1,10 @@
+class Level1 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello level 1");
+    }
+
+    private int add(int a, int b) {
+        return a + b;
+    }
+}
